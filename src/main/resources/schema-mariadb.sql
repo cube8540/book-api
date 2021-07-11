f54fd2b93210e40f1bc9fbdb90909396f13f4e9d
cube8540 <cube8540@gmail.com>
2021-07-11 18:23:34 +0900

    mariadb on update 이벤트 삭제

--- src/main/resources/schema-mariadb.sql
+++ src/main/resources/schema-mariadb.sql
@@ -4,6 +4,8 @@ create table if not exists publishers (
     created_at timestamp not null,
     updated_at timestamp not null
 );
+alter table publishers change created_at created_at timestamp not null default current_timestamp;
+alter table publishers change updated_at updated_at timestamp not null default current_timestamp;
 
 create table if not exists books (
     isbn varchar(13) not null primary key,
@@ -23,6 +25,8 @@ create table if not exists books (
 create index if not exists book_publish_date_index on books (publish_date);
 create index if not exists book_series_code_index on books (series_code);
 create index if not exists book_series_isbn_index on books (series_isbn);
+alter table books change column created_at created_at timestamp not null default current_timestamp;
+alter table books change column updated_at updated_at timestamp not null default current_timestamp;
 
 create table if not exists book_authors (
     isbn varchar(13) not null,
